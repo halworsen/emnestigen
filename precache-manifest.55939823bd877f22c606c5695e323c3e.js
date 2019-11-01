15c64b9cc953b1c734599ae7f2ce1fdba1be3636
Markus <markus@halvorsenfamilien.com>
2019-11-01 17:53:22 +0100

    Updates

---
+++ precache-manifest.55939823bd877f22c606c5695e323c3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1144eab92e166aac1f1a9e96b5711ed",
+    "url": "/emnestigen/index.html"
+  },
+  {
+    "revision": "274534c0aa414cb3f746",
+    "url": "/emnestigen/static/css/main.78d38d51.chunk.css"
+  },
+  {
+    "revision": "b6b0e67176afb50a831a",
+    "url": "/emnestigen/static/js/2.ec3728f1.chunk.js"
+  },
+  {
+    "revision": "274534c0aa414cb3f746",
+    "url": "/emnestigen/static/js/main.9bf92cc6.chunk.js"
+  },
+  {
+    "revision": "8b0f20caeb87e1e5f621",
+    "url": "/emnestigen/static/js/runtime-main.9dec440e.js"
+  }
+]);
